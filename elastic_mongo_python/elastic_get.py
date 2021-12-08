d2ce9523b0574fe288791feef86eaf6a666e8703
vikas <vik27654@gmail.com>
2021-12-09 03:14:09 +0530

    initial commit

--- elastic_mongo_python/elastic_get.py
+++ elastic_mongo_python/elastic_get.py
@@ -6,11 +6,12 @@ url = "http://localhost:8080/mongo/operation?collectionName=Url&OperationType=in
 headers = {
         'Content-Type': 'application/json',
     }
-data = '{  "query": {"match_all": {}  },  "from": 1000,  "size":10 ,  "_source": "url"}'
+data = '{  "query": {"match_all": {}  },  "from": 10000,  "size":1000,  "_source": "url"}'
 response = requests.get('http://localhost:9200/status/_search', headers=headers, data=data)
 temp = response.json()
 # pprint(temp)
-for d in temp["hits"]["hits"]:
+for d in temp['hits']['hits']:
     pprint(d)
+    time.sleep(1)
     response1 = requests.post(url, json=d)
 # time.sleep(50)
