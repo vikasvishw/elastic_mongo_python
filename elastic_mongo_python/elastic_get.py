0657444682ffcf138ada8b4a20ed40325e676096
vikas <vik27654@gmail.com>
2021-12-11 04:04:37 +0530

    initial commit

--- elastic_mongo_python/elastic_get.py
+++ elastic_mongo_python/elastic_get.py
@@ -1,4 +1,5 @@
 import time
+from contextlib import redirect_stderr
 from pprint import pprint
 import requests
 
@@ -6,12 +7,12 @@ url = "http://localhost:8080/mongo/operation?collectionName=Url&OperationType=in
 headers = {
         'Content-Type': 'application/json',
     }
-data = '{  "query": {"match_all": {}  },  "from": 10000,  "size":1000,  "_source": "url"}'
+data = '{  "query": {"match_all": {}  },  "from": 0,  "size":1000,  "_source": "url"}'
 response = requests.get('http://localhost:9200/status/_search', headers=headers, data=data)
 temp = response.json()
 # pprint(temp)
 for d in temp['hits']['hits']:
-    pprint(d)
-    time.sleep(1)
+    pprint(d["_source"])
+    # time.sleep(1)
     response1 = requests.post(url, json=d)
 # time.sleep(50)
